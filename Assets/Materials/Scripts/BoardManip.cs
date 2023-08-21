a0a7c9d8705b6c1728e2902f0cc3c5a13d1a78e7
SmolinIV <Smolin1992@bk.ru>
2023-08-21 23:16:53 +0500

    Try to add sorting Function

--- Assets/Materials/Scripts/BoardManip.cs
+++ Assets/Materials/Scripts/BoardManip.cs
@@ -15,8 +15,18 @@ public class BoardManip : MonoBehaviour
     [HideInInspector] public int emptyCubeGridPosition;
     [HideInInspector] public int slidingCubeNumber;
 
+    [HideInInspector] public int numberOfSwapingCubes;
+    [HideInInspector] public bool sortingDone;
+
+    enum DIRECTION
+    {
+        UP, DOWN, LEFT, RIGHT, NONE
+    }
+
     public void Awake()
     {
+        sortingDone = false;
+        numberOfSwapingCubes = 50;
         alreadyMoving = false;
         numberOfCubes = _cubeObjects.Length;
         cubes = new MovingCube[numberOfCubes];
@@ -29,13 +39,65 @@ public class BoardManip : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log(emptyCubeGridPosition);
-        Debug.Log(_cubeObjects.Length);
+
     }
 
     void Update()
     {
+        if (sortingDone)
+        {
+            for (int i = 0; i < numberOfCubes; ++i)
+            {
+                cubes[i].transform.position = cubes[i].cubePositionOnScene;
+            }
+            sortingDone = false;
+        }
+    }
 
+    public void SortingBoard()
+    {
+        DIRECTION dir;
+        DIRECTION lastDir = DIRECTION.NONE;
+        System.Random rand = new System.Random();
+
+
+        for (int i = 0; i < numberOfSwapingCubes; ) {
+            dir = (DIRECTION)rand.Next(0,3);
+            if (dir == lastDir) { continue; }
+            switch (dir)
+            {
+                case DIRECTION.UP:
+                    if (emptyCubeGridPosition <= 3) { continue; }
+                    swapCubes(emptyCubeGridPosition, emptyCubeGridPosition - 4);
+                    i++;
+                    break;
+                case DIRECTION.DOWN:
+                    if (emptyCubeGridPosition >= 11) { continue; }
+                    swapCubes(emptyCubeGridPosition, emptyCubeGridPosition + 4);
+                    i++;
+                    break;
+                case DIRECTION.LEFT:
+                    if (emptyCubeGridPosition % 4 == 0) { continue; }
+                    swapCubes(emptyCubeGridPosition, emptyCubeGridPosition -1);
+                    i++;
+                    break;
+                case DIRECTION.RIGHT:
+                    if ((emptyCubeGridPosition + 1) % 4 == 0) { continue; }
+                    swapCubes(emptyCubeGridPosition, emptyCubeGridPosition + 1);
+                    i++;
+                    break;
+            }
+            lastDir = dir;
+        }
+
+        sortingDone = true;
+    }
+
+    private void swapCubes(int c1, int c2)
+    {
+        (cubes[c1].positionOnGrid, cubes[c2].positionOnGrid) = (cubes[c2].positionOnGrid, cubes[c1].positionOnGrid);
+        (cubes[c1].cubePositionOnScene, cubes[c2].cubePositionOnScene) = (cubes[c2].cubePositionOnScene, cubes[c1].cubePositionOnScene);
+        (cubes[c1], cubes[c2]) = (cubes[c2], cubes[c1]);
+        emptyCubeGridPosition = c2;
     }
 }
-
